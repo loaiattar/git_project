551992f57257a202cfb5bf68e8b9260484739ead
loaiattar <loaiattar@gmail.com>
2024-11-30 16:04:25 +0100

    adding status to my todo

--- exercice.py
+++ exercice.py
@@ -10,3 +10,13 @@ def lister_todos():
     for index, todo in enumerate(todos, 1):
         print(f"{index}: {todo['titre']} - Statut: {todo['statut']}")
     print("=========================")
+
+# Fonction pour créer un todo avec statut "À faire"
+def creer_todo():
+    titre = input("Entrez le titre du todo: ")
+    todo = {
+        'titre': titre,
+        'statut': 'À faire'  # Par défaut, le statut est "À faire"
+    }
+    todos.append(todo)
+    print(f"Todo '{titre}' créé avec le statut 'À faire'..")
